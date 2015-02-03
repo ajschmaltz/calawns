70b9218238805a524adb3c7e308f1312d8aa804b
luscadigital <joey@luscadigital.com>
2015-02-02 21:47:33 -0500

    nav site name to home

--- resources/views/block/nav.blade.php
+++ resources/views/block/nav.blade.php
@@ -8,7 +8,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">C.A. Lawns</a>
+      <a class="navbar-brand" href="/">C.A. Lawns</a>
     </div>
     
     <form class="navbar-form navbar-right text-center">
