e95a53cad904fcad6673552a40b1274f3f087141
luscadigital <joey@luscadigital.com>
2015-02-02 21:16:00 -0500

    move to mandrill

--- resources/views/emails/quote.blade.php
+++ resources/views/emails/quote.blade.php
@@ -1,1 +1,7 @@
-Testing
+<ul>
+  <li>Name: {{ $name }}</li>
+  <li>Phone: {{ $phone }}</li>
+  <li>Address: {{ $address }}</li>
+  <li>Service: {{ $service }}</li>
+  <li>Details: {{ $details }}</li>
+</ul>
